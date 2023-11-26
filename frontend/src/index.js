24caf5323c756fbaf7b0ae18aa64a056146864e6
pookey1104 <pookey1104@naver.com>
2023-11-27 00:56:38 +0900

    [1127] totree 사이트&이미지 #서윤

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./styles/index.css";
 import App from "./App";
+import { Helmet } from "react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <div>
+      <Helmet>
+        <title>ToTree</title>
+      </Helmet>
+    </div>
   </React.StrictMode>
 );
