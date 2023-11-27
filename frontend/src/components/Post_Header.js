8edaa8c78ae755c95ac0b15759ac576a06ab19ac
pookey1104 <pookey1104@naver.com>
2023-11-28 03:09:41 +0900

    [1127] main post 구현 #서윤

---
+++ frontend/src/components/Post_Header.js
@@ -0,0 +1,15 @@
+// import "../styles/Header.css";
+
+import back from "../img/back.png";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="hd-p">
+        <p>마음을 꼭 담아 전달해주세요.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
