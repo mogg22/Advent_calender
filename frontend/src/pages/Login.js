34b4441d84deaddcda2fbfeb6579447fbf03d189
lsk808 <160msh@naver.com>
2023-11-29 14:21:35 +0900

    [1129] 페이지 생성 완료, 편집중

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,6 +6,21 @@ function Login() {
       <div className="page-bg">
         <div className="center">
           <div className="white-door"></div>
+          <div className="title"></div>
+          <div className="login-content">
+            <div className="ID">
+              <p>아이디</p>
+              <form></form>
+            </div>
+            <div className="password">
+              <p>비밀번호</p>
+              <form></form>
+            </div>
+            <div className="tosend">
+              <p>누구에게 편지를 보내고 싶은가요?</p>
+              <form></form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
