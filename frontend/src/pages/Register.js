aff222dcf760ddffa53865b93214e0d1df2b3517
lsk808 <160msh@naver.com>
2023-11-30 02:52:41 +0900

    [1130] 로그인, 회원가입, 맨처음 페이지

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,11 +1,35 @@
-// import "../styles/Register.css";
+import "../styles/Register.css";
+import Title from "../img/title.png";
 
 function Register() {
   return (
     <div className="start-page">
       <div className="page-bg">
         <div className="center">
-          <div className="white-door"></div>
+          <div className="register-content">
+            <div className="title">
+                <img src={Title}/>
+            </div>
+            <div className="name">
+              <p>닉네임</p>
+              <input className="blank" type="text" />
+            </div>
+            <div className="password">
+              <p>비밀번호</p>
+              <input className="blank" type="password" />
+            </div>
+            <div className="check">
+              <p>비밀번호 확인</p>
+              <input className="blank" type="password" />
+            </div>
+            <div className="tosend">
+              <p>누구에게 트리를 보낼까요?</p>
+              <input className="blank" type="text" />
+            </div>
+            <div className="submit">
+              <p>회원가입</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
