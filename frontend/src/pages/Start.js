581e4212f99880c5c233144fdcd1d6b8cebdb0a7
lsk808 <160msh@naver.com>
2023-11-29 11:41:58 +0900

    [1129] 큰 이미지 추가

--- frontend/src/pages/Start.js
+++ frontend/src/pages/Start.js
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
 import "../styles/Start.css";
+import Title from "../img/title.png";
 
 function Start() {
   return (
     <div className="start-page">
       <div className="page-bg">
         <div className="center">
+          <div className="start-content">
+            <div className="start-bg">
+              <div className="title">
+                <img src={Title}/>
+              </div>
+              <div className="start-btn">
+                <div className="login-btn">
+                  <Link to="/Login"><p>로그인</p></Link>
+                  </div>
+                <div className="register-btn">
+                <Link to="/Register"><p>회원가입</p></Link>
+                  </div>
+              </div>
+            </div>
+          </div>
           <div className="white-door"></div>
         </div>
       </div>
