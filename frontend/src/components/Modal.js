fb425c2abc979e58b0ea8656a6c64c16f459fd04
pookey1104 <pookey1104@naver.com>
2023-11-30 04:01:18 +0900

    [1130] main&modal 수정 #서윤

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react";
-import ornamentImage from "../img/ornament20.png";
+import React, { useEffect, useState } from "react";
+// import ornamentImage from "../img/ornament20.png";
 import "../styles/Modal.css";
 
-function Modal({ onClose }) {
+function Modal({ onClose, boxNumber }) {
+  const [ornamentImage, setOrnamentImage] = useState("");
+
   useEffect(() => {
+    // 박스 번호에 해당하는 이미지 설정
+    setOrnamentImage(boxNumber || "/path/to/default/image"); // 만약 boxNumber가 falsy하면 기본 이미지 경로를 사용합니다
+
     const timeoutId = setTimeout(() => {
       onClose();
-      // Redirect to "/main/post" after 1.5 second
+      // 1.5초 후에 "/main/post"로 리다이렉트
       window.location.href = "/main/post";
     }, 1500);
 
     return () => clearTimeout(timeoutId);
-  }, [onClose]);
+  }, [onClose, boxNumber]);
 
   return (
     <div className="modal-overlay">
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {/* <p>박스 번호: {boxNumber}</p> */}
         <img src={ornamentImage} alt="Ornament" />
-        {/* You can add additional content or styling as needed */}
+        {/* 필요에 따라 내용이나 스타일을 추가하세요 */}
       </div>
     </div>
   );
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -4,6 +4,34 @@ import { Link } from "react-router-dom";
 import Modal from "../components/Modal";
 import "../styles/Main.css";
 
+import ornament1 from "../img/ornament_big1.png";
+import ornament2 from "../img/ornament_big2.png";
+import ornament3 from "../img/ornament_big3.png";
+import ornament4 from "../img/ornament_big4.png";
+import ornament5 from "../img/ornament_big5.png";
+import ornament6 from "../img/ornament_big6.png";
+import ornament7 from "../img/ornament_big7.png";
+import ornament8 from "../img/ornament_big8.png";
+import ornament9 from "../img/ornament_big9.png";
+import ornament10 from "../img/ornament_big10.png";
+import ornament11 from "../img/ornament_big11.png";
+import ornament12 from "../img/ornament_big12.png";
+import ornament13 from "../img/ornament_big13.png";
+import ornament14 from "../img/ornament_big14.png";
+import ornament15 from "../img/ornament_big15.png";
+import ornament16 from "../img/ornament_big16.png";
+import ornament17 from "../img/ornament_big17.png";
+import ornament18 from "../img/ornament_big18.png";
+import ornament19 from "../img/ornament_big19.png";
+import ornament20 from "../img/ornament_big20.png";
+import ornament21 from "../img/ornament_big21.png";
+import ornament22 from "../img/ornament_big22.png";
+import ornament23 from "../img/ornament_big23.png";
+import ornament24 from "../img/ornament_big24.png";
+import ornament25 from "../img/ornament_big25.png";
+
+const ornamentImages = [ornament1, ornament2, ornament3, ornament4, ornament5, ornament6, ornament7, ornament8, ornament9, ornament10, ornament11, ornament12, ornament13, ornament14, ornament15, ornament16, ornament17, ornament18, ornament19, ornament20, ornament21, ornament22, ornament23, ornament24, ornament25];
+
 function Main() {
   const boxNumbers = Array.from({ length: 25 }, (_, index) => index + 1);
   const chunkedBoxNumbers = [];
@@ -12,27 +40,43 @@ function Main() {
   }
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
   const [currentDate, setCurrentDate] = useState("");
 
   useEffect(() => {
-    // Function to get the current date in the format `${month}.${day}`
+    // 컴포넌트가 마운트될 때 현재 날짜를 설정하는 함수
     const getCurrentDate = () => {
       const date = new Date();
-      const month = date.getMonth() + 1; // Months are zero-indexed
+      const month = date.getMonth() + 1; // 월은 0부터 시작합니다
       const day = date.getDate();
       return `${month}.${day}`;
     };
 
-    // Set the current date when the component mounts
+    // 컴포넌트가 마운트될 때 현재 날짜 설정
     setCurrentDate(getCurrentDate());
   }, []);
 
-  const openModal = () => {
+  // 열린 박스를 추적하기 위한 상태 변수
+  const [openedBoxes, setOpenedBoxes] = useState([]);
+
+  // 박스가 이미 열렸는지 확인하는 함수
+  const isBoxOpened = (boxNumber) => openedBoxes.includes(boxNumber);
+
+  // 모달 열기 함수 수정
+  const openModal = (boxNumber) => {
     setIsModalOpen(true);
+
+    // 박스 번호에 해당하는 이미지 경로를 모달 컴포넌트에 전달
+    setModalContent(ornamentImages[boxNumber - 1]);
+
+    // 열린 박스 상태 업데이트
+    setOpenedBoxes([...openedBoxes, boxNumber]);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
+    // 모달이 닫힐 때 모달 내용 초기화
+    setModalContent(null);
   };
 
   return (
@@ -53,7 +97,7 @@ function Main() {
                   {chunkedBoxNumbers.map((row, rowIndex) => (
                     <div key={rowIndex} className="box-row">
                       {row.map((number) => (
-                        <div key={number} className="box" onClick={() => openModal(number)}>
+                        <div key={number} className={`box ${isBoxOpened(number) ? "box-opened" : ""}`} onClick={() => openModal(number)}>
                           <p>{number}</p>
                         </div>
                       ))}
@@ -71,7 +115,7 @@ function Main() {
         </div>
       </div>
 
-      {isModalOpen && <Modal onClose={closeModal} />}
+      {isModalOpen && <Modal onClose={closeModal} boxNumber={modalContent} />}
     </div>
   );
 }
