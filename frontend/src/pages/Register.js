b524df3eaf62a0fb3283bba9446248479a70ee68
pookey1104 <pookey1104@naver.com>
2023-12-04 22:22:01 +0900

    [1204] FE-BE 연동1 #서윤

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,32 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "../styles/Register.css";
 import Title from "../img/title.png";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    nickname: "",
+    password: "",
+    password2: "",
+    receiver: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/api/users/signup", formData, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 200) {
+        // Handle successful registration
+        console.log("User registered successfully");
+      } else {
+        // Handle registration failure
+        console.error("Error registering user");
+      }
+    } catch (error) {
+      console.error("Error registering user", error);
+    }
+  };
+
   return (
     <div className="start-page">
       <div className="page-bg">
         <div className="center">
           <div className="register-content">
             <div className="title">
-                <img src={Title}/>
+              <img src={Title} />
             </div>
             <div className="name">
               <p>닉네임</p>
-              <input className="blank" type="text" />
+              <input className="blank" type="text" name="nickname" onChange={handleInputChange} />
             </div>
             <div className="password">
               <p>비밀번호</p>
-              <input className="blank" type="password" />
+              <input className="blank" type="password" name="password" onChange={handleInputChange} />
             </div>
             <div className="check">
               <p>비밀번호 확인</p>
-              <input className="blank" type="password" />
+              <input className="blank" type="password" name="password2" onChange={handleInputChange} />
             </div>
             <div className="tosend">
               <p>누구에게 트리를 보낼까요?</p>
-              <input className="blank" type="text" />
+              <input className="blank" type="text" name="receiver" onChange={handleInputChange} />
             </div>
-            <div className="submit">
+            <div className="submit" onClick={handleSignUp}>
               <p>회원가입</p>
             </div>
           </div>
