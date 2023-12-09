fcb45001c932de8b7b63687139d388eb6968c264
pookey1104 <pookey1104@naver.com>
2023-12-09 11:43:33 +0900

    [1209] header ticket css 수정 및 api 연동 #서윤

--- frontend/src/components/Main_Header.js
+++ frontend/src/components/Main_Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import "../styles/Main_Header.css";
 
@@ -9,6 +9,25 @@ import ticket from "../img/ticket.png";
 
 function Main_Header() {
   const navigate = useNavigate();
+  const [ticketCount, setTicketCount] = useState(0);
+
+  const location = useLocation();
+  const { accessToken } = location.state || { accessToken: null };
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        // 유저 정보 가져오기 API 호출
+        const response = await axios.get("http://localhost:8080/api/users/user-info");
+        const userTicketCount = response.data.ticket; // 티켓 개수 추출
+        setTicketCount(userTicketCount);
+      } catch (error) {
+        console.error("유저 정보 가져오기 에러:", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
 
   const handleLogout = async () => {
     try {
@@ -29,23 +48,23 @@ function Main_Header() {
     }
   };
 
+  const handleMypageLink = () => {
+    navigate("/mypage", { state: { accessToken } });
+  };
+
   return (
     <div className="Header">
       <div className="hd-img">
         <div className="setting-ticket">
-          <Link to="/mypage">
-            <img src={setting} alt="설정" />
-          </Link>
+          <img src={setting} alt="설정" onClick={handleMypageLink} />
           <div className="ticket-box">
             <div className="ticket-img">
               <img src={ticket} alt="티켓" />
             </div>
-            <p>3</p>
+            <p>{ticketCount}</p>
           </div>
         </div>
-        <Link to="/">
-          <img src={logout} alt="로그아웃" onClick={handleLogout} />
-        </Link>
+        <img src={logout} alt="로그아웃" onClick={handleLogout} />
       </div>
     </div>
   );
